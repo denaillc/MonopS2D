3a00aa69212b78b8b4dfc8cd3912d237b7ec4840
albertar <albertar@pc-dg-037-13>
2016-12-08 15:01:38 +0100

    Merge origin/master
    Conflicts:
    	src/Controleur/Controleur.java

--- src/UI/Ihm.java
+++ src/UI/Ihm.java
@@ -11,11 +11,11 @@ import java.util.*;
  *
  * @author albertar
  */
-public class Ihm extends Observable{    
-     private static int nbJoueurs;
-     private static String nomJoueur;
+public class Ihm extends Observable{
+    private int nbJoueurs =0;
+    private String nomJoueur;
     
-   public void nbJoueur() {
+   /*public void nbJoueur() {
         int c = 0;
         System.out.print("Inscrire le nombre de joueur : ");
         while (c > 6 || c < 2) {
@@ -25,31 +25,51 @@ public class Ihm extends Observable{
             } catch (InputMismatchException e) {
                 System.err.println("Entrer le nombre de joueurs sous format numérique (différent de 0) : ");
             }
-            if (c > 6 || c < 2) {
+            if (c > 6 || c< 2) {
                 System.out.print("Entrer un nombre de joueurs entre 2 et 6 :");
+                System.out.println(c);
             }
         }
-        
         setChanged();
-        notifyObservers(c);
+        notifyObservers(Validation.ValiderNombreJoueur);
         clearChanged();
-    }
+    }*/
+   
+   
+   public void nbJoueur() {
+       System.out.print("Inscrire le nombre de joueurs : ");
+       while (this.getNbJoueurs() > 6 || this.getNbJoueurs() < 2) {
+           try {
+               Scanner sc = new Scanner(System.in);
+               this.setNbJoueurs(sc.nextInt());
+           }
+           catch (InputMismatchException e) {
+               System.err.println("*Entrer le nombre de joueurs au format numérique (entre 2 et 6) : ");
+           }
+       }
+       System.out.println("getNbJoueurs: " + this.getNbJoueurs());
+   }
     
     public void nomJoueur(){
         Scanner sc = new Scanner(System.in);
             System.out.print("Entrez le nom du joueur : ");
             String nom = sc.nextLine();
             System.out.println("\n");
-            
             setChanged();
             notifyObservers(nom);
             clearChanged();
     }
+
     
-    public static void CreationJoueurs(){
-        
-            
+    
+        public void abonner(Observer ecoutant) {
+        this.addObserver(ecoutant);
     }
+
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////GUETTEUR&SETTEUR/////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
 
     public int getNbJoueurs() {
         return nbJoueurs;
@@ -67,5 +87,6 @@ public class Ihm extends Observable{
         this.nomJoueur = nomJoueur;
     }
     
+ 
     
 }
